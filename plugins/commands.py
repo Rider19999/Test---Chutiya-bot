5899d7c416d4cb689436a0bb33c51de215e116f6
Deendayal <128588969+deendayal403@users.noreply.github.com>
2024-06-19 13:09:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,13 +248,13 @@ async def start(client, message):
         if str(message.from_user.id) != str(userid):
             return await message.reply_text(
                 text="<b>Invalid link or Expired link !</b>",
-                protect_content=True
+                protect_content=False
             )
         is_valid = await check_token(client, userid, token)
         if is_valid == True:
             await message.reply_text(
                 text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all movies till today midnight.</b>",
-                protect_content=True
+                protect_content=False
             )
             await verify_user(client, userid, token)
         else:
