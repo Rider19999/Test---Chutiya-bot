ba65193de0b3ec4681347e1301b8867aff56269d
SS movie <128588969+SSmovie@users.noreply.github.com>
2024-07-06 19:50:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -26,7 +26,7 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('🔰 Add Me To Your Group 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('⚜️ Contact ⚜️', url="https://t.me/Contact_Deendayal_bot"),
+                    InlineKeyboardButton('⚜️ Trending ⚜️', url="topsearch"),
                     InlineKeyboardButton('🔱 Movie Group 🔱', url='https://t.me/Deendayal_Movies_Group')
                 ],[
                   InlineKeyboardButton('💖 Chat Gpt Bot 💖', url='https://t.me/Deendayal_Chat_Gpt_Bot')
